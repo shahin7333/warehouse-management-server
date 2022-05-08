f0296c1bf0bef21c00485780e01547ab67c2ef58
shaparan shahin <shaparan7333@gmail.com>
2022-05-08 16:16:54 +0600

    first

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const app = express()
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5wev9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+
+async function run() {
+  try {
+    await client.connect()
+    const productCollection = client
+      .db('bicycleWarehouse')
+      .collection('inventory')
+    app.get('/inventory', async (req, res) => {
+      const query = {}
+      const cursor = productCollection.find(query)
+      const inventories = await cursor.toArray()
+      res.send(inventories)
+    })
+     app.get('/inventory/:id',async (req, res)=>{
+         const id=req.params.id;
+         const query={_id: ObjectId(id)};
+         const inventory=await productCollection.findOne(query);
+         res.send(inventory);
+     })
+
+  } 
+  finally {
+  }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('running my bicycle server')
+})
+
+app.listen(port, () => {
+  console.log('Listening to port', port)
+})
